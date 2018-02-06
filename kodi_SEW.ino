fe9d9efd4d8d03c9b75a132fbcfbcb08afcd8afb
SedKol12 <sedkol12@htl-shkoder.com>
2018-02-06 23:49:05 +0100

    Add files via upload

--- kodi_SEW.ino
+++ kodi_SEW.ino
@@ -4,7 +4,7 @@
  Servo servo2;
  Servo servo3;
  Servo servo4;
-
+ int a;
 void setup() {
          servo1.attach(5);
          servo2.attach(6);
@@ -15,7 +15,7 @@ void setup() {
          pinMode(11, OUTPUT); // I jep energji motorit
 }
 
-void levizja(){
+void sektor1(){
     
           servo1.write(45); //hapet
            delay(2000);
@@ -30,14 +30,57 @@ void levizja(){
           servo4.write(180); //rrotullohet
            delay(2000);
     
-      digitalWrite(13, HIGH); //Vendos drejtimin para te motorit
-      digitalWrite(8, LOW);   //Caktvizon frenat
-      analogWrite(11,80);
-       delay(3000);
+     digitalWrite(13, HIGH); //Vendos drejtimin para te motorit
+     digitalWrite(8, LOW);   //Caktvizon frenat
+     analogWrite(11,80);
+      delay(3000);
           
       //Procesi i ndaljes
-      digitalWrite(8, HIGH); //aktivizon frenat
-       delay(1000);
+     digitalWrite(8, HIGH); //aktivizon frenat
+      delay(1000);
+  
+            servo3.write(145); //krahu ulet
+             delay(3000);
+            servo1.write(45); //hapet
+             delay(2000);
+            servo3.write(60); // cohet
+             delay(5000);
+            servo4.write(0); //rrotullohet
+             delay(2000);
+ 
+     digitalWrite(13, LOW); //vendos drejtimin prapa te motorit
+     digitalWrite(8, LOW);   //Caktivizon frenat
+     analogWrite(11,150);
+      delay(3000);
+         
+     digitalWrite(8, HIGH); //aktivizon frenat
+      delay(1000);
+
+}
+
+void sektor2(){
+    
+          servo1.write(45); //hapet
+           delay(2000);
+          servo3.write(148); //krahu ulet
+           delay(3000);
+          servo2.write(60); // ngrihet
+           delay(2000);
+          servo1.write(84); //mbyllet
+           delay(2000);
+          servo3.write(60); // cohet
+           delay(5000);
+          servo4.write(180); //rrotullohet
+           delay(2000);
+    
+     digitalWrite(13, HIGH); //Vendos drejtimin para te motorit
+     digitalWrite(8, LOW);   //Caktvizon frenat
+     analogWrite(11,80);
+      delay(3000);
+          
+      //Procesi i ndaljes
+     digitalWrite(8, HIGH); //aktivizon frenat
+      delay(1000);
   
          servo3.write(145); //krahu ulet
           delay(3000);
@@ -48,18 +91,27 @@ void levizja(){
          servo4.write(0); //rrotullohet
           delay(2000);
  
-      digitalWrite(13, LOW); //vendos drejtimin prapa te motorit
-      digitalWrite(8, LOW);   //Caktivizon frenat
-      analogWrite(11,150);
-       delay(3000);
+     digitalWrite(13, LOW); //vendos drejtimin prapa te motorit
+     digitalWrite(8, LOW);   //Caktivizon frenat
+     analogWrite(11,150);
+      delay(3000);
          
-      digitalWrite(8, HIGH); //aktivizon frenat
-       delay(1000);
+     digitalWrite(8, HIGH); //aktivizon frenat
+      delay(1000);
 
 }
     
 void loop() {
-levizja();
+  
+ if(a<=10)
+ {
+ sektor1();
+ }
+ else if(a>15)
+ {
+  sektor2();
+ }
+ 
 }
 
 
